89abd6b9cbdc415964c6c78f228e42ba788fd85a
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-06-14 21:46:17 +0530

    Add files via upload

---
+++ registration_login_rowcol.php
@@ -0,0 +1,15 @@
+
+
+
+
+
+<h1> Student Registration Form</h1>
+
+
+First name <input type='text' nam='t1'  className= ><br>
+First name<input type='text' nam='t1'  className= ><br>
+First name<input type='text' nam='t1'  className= ><br>
+Course:<input type='text' nam='t1'  className= ><br>
+Gender:<input type='text' nam='t1'  className= ><br>
+
+
